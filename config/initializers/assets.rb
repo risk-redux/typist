22cc8a3152f7d6f17b8782ca1eb6dbef7b20491c
Karim Said <karim@egyptiankarim.com>
2016-04-11 12:57:06 -0400

    Initial commit.

---
+++ config/initializers/assets.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += ['glyphicons-halflings-regular.*']
+
+# To support controller specific css/js rendering
+Rails.application.config.assets.precompile += ['*.css']
+Rails.application.config.assets.precompile += ['*.js']
+
+# All my lib images
+Rails.application.config.assets.precompile += ['*.png']
