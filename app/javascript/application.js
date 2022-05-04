ee546c9d3f7785e7541cf5abaf23fe47e9a90bff
Karim Said <karim.a.said@nasa.gov>
2022-05-04 12:47:40 -0400

    Tweaking asset pipeline.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-$(document).ready(function() {
-    $('tr[data-link]').click(function() {
-      const href = $(this).attr('data-link');
-      if (href) {
-        window.location = href;
-      }
-    });
-  });
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,12 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+$(document).ready(function() {
+  $('tr[data-link]').click(function() {
+    const href = $(this).attr('data-link');
+    if (href) {
+      window.location = href;
+    }
+  });
+});
